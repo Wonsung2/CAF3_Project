47c672838769bb4d7d27b425e7a4ca5b079d0140
Jason <jaewoo.choi@shms-mail.ch>
2022-05-13 15:37:07 +0900

    fixed static file

--- ChatWeb/MainWeb/urls.py
+++ ChatWeb/MainWeb/urls.py
@@ -6,7 +6,8 @@ from MainWeb        import views
 urlpatterns = [
     path('index/', views.index, name = 'index'),
     path('test/', views.test, name='test'),
-    path('chatanswer', views.chatanswer, name='chatanswer'),
-
+    path('chattrain', views.chattrain, name='chattrain'),
+    path('example', views.example, name='example'),
+    path('example2', views.example2, name='example2'),
 
 ]
