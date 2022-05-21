84fbc05f9417b4c380f4a340b0f130f3f5cc56df
Wonsung2 <q92235@naver.com>
2022-05-21 17:43:11 +0900

    찐막

--- ChatWeb/MainWeb/views.py
+++ ChatWeb/MainWeb/views.py
@@ -2,99 +2,33 @@ from django.shortcuts import render
 from django.http import JsonResponse,HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import tensorflow as tf
-import numpy as np
 import pandas as pd
 import os
 import re
 import urllib.request
-import time
 import json
-import tensorflow as tf
-from tensorflow.keras.models import Sequential, clone_model, Model, load_model
-from tensorflow.keras.layers import Dense, Activation,InputLayer, Flatten, Input, BatchNormalization,Dropout,Embedding  # Dense란 하나의 뉴럴 층
-from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
-
-import tensorflow.keras
 import pickle
 import numpy as np
-import pandas as pd
-import os
-import re
-import urllib.request
-import time
-import json
 from tensorflow import keras
-
 from tensorflow.keras.models import Sequential, clone_model, Model, load_model
 from tensorflow.keras.layers import Dense, Activation,InputLayer, Flatten, Input, BatchNormalization,Dropout,Embedding  # Dense란 하나의 뉴럴 층
-from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
-
-
+from sklearn.preprocessing import LabelEncoder
 # CNN
 from tensorflow.keras.layers import Conv2D,MaxPooling2D,AveragePooling2D, GlobalAveragePooling1D
-
 # RNN
 from tensorflow.keras.layers import SimpleRNN, LSTM
-
-from tensorflow.keras.datasets import boston_housing, mnist, fashion_mnist, reuters
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, Callback
 from tensorflow.keras import optimizers
 from tensorflow.keras.optimizers import SGD,Adam,RMSprop
-
-
-# 이미지 로드
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
+from tensorflow.keras.utils import to_categorical
 # 자연어 처리
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-
-
-from sklearn.datasets import load_iris, load_breast_cancer, load_digits
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import LabelEncoder
-
-
-
-# CNN
-from tensorflow.keras.layers import Conv2D,MaxPooling2D,AveragePooling2D, GlobalAveragePooling1D
-
-# RNN
-from tensorflow.keras.layers import SimpleRNN, LSTM
-
-from tensorflow.keras.datasets import boston_housing, mnist, fashion_mnist, reuters
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, Callback
-from tensorflow.keras import optimizers
-from tensorflow.keras.optimizers import SGD,Adam,RMSprop
-from tensorflow.keras.utils import to_categorical  # onehotencoding해주는 거
-
-# 이미지 로드
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.preprocessing.image import ImageDataGenerator  # image에 대한 전체적인 전처리를 진행시켜준다.
-
-# 자연어 처리
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-
-
-from sklearn.datasets import load_iris, load_breast_cancer, load_digits
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import LabelEncoder
-
 from konlpy.tag import Okt
-
 import colorama
 colorama.init()
 from colorama import Fore, Style, Back
 import random
-
-
-# import matplotlib.pyplot as plt
-
-
 
 
 # Create your views here.
